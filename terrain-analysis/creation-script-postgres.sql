858c3bcbdf917a01f2f6ef0dcd093c2a31b9b1ea
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2019-12-02 00:45:41 +0100

    add terrain analysis

---
+++ terrain-analysis/creation-script-postgres.sql
@@ -0,0 +1,21 @@
+create database visitors;
+
+CREATE USER brooklyn WITH ENCRYPTED PASSWORD 'br00k11n';
+
+GRANT CONNECT ON DATABASE visitors TO brooklyn;
+GRANT ALL PRIVILEGES ON DATABASE visitors TO brooklyn;
+
+\connect visitors
+
+GRANT ALL PRIVILEGES ON SCHEMA public TO brooklyn;
+
+CREATE TABLE public.MESSAGES (
+                                 ID SERIAL PRIMARY KEY,
+                                 NAME VARCHAR(30) NOT NULL,
+                                 MESSAGE VARCHAR(400) NOT NULL
+);
+
+GRANT ALL PRIVILEGES ON  ALL TABLES IN SCHEMA  public TO brooklyn;
+GRANT ALL PRIVILEGES ON  ALL SEQUENCES IN SCHEMA  public TO brooklyn;
+
+INSERT INTO MESSAGES(NAME, MESSAGE) values ( 'J. C. Wylie', 'A plan of action designed in order to achieve some end; a purpose together with a system of measures for its accomplishment.' );
