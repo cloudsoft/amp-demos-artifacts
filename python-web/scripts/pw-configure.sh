963a91760871798c0944e45894162ba7bcb497cd
Duncan Grant <duncan.grant@cloudsoft.io>
2021-08-05 15:52:25 +0100

    python scripts

--- python-web/scripts/pw-configure.sh
+++ python-web/scripts/pw-configure.sh
@@ -3,11 +3,11 @@ cat > /tmp/index.html << eof
 
   hello world.
   <p>
-  i am ${hostname}
+  i am ${HOSTNAME}
   <p>
   created at: `date`
   <p>
-  i am running at ${hostname}, with on-box ip configuration:
+  i am running at ${HOSTNAME}, with on-box ip configuration:
   <pre>
   `ifconfig | grep inet`
   </pre>
