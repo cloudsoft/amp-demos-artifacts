3b901114ee305058b4ed0adc21ff08025e9036c9
iuliana <iuliana@cloudsoft.io>
2021-11-12 13:01:33 +0000

    Updates sql for tosca

--- tosca/creation-script-mysql.sql
+++ tosca/creation-script-mysql.sql
@@ -23,4 +23,7 @@ CREATE TABLE MESSAGES (
                           PRIMARY KEY (ID)
 );
 
-INSERT INTO MESSAGES(NAME, MESSAGE) values ('Isaac Asimov', 'I hope you took your vitamins' );
+INSERT INTO MESSAGES(NAME, MESSAGE) values ('Isaac Asimov', 'I was born in Brooklyn' );
+INSERT INTO MESSAGES(NAME, MESSAGE) values ('Isaac Asimov', 'A robot may not injure a human being or, through inaction, allow a human being to come to harm.' );
+INSERT INTO MESSAGES(NAME, MESSAGE) values ('Isaac Asimov', 'A robot must obey the orders given it by human beings except where such orders would conflict with the First Law.' );
+INSERT INTO MESSAGES(NAME, MESSAGE) values ('Isaac Asimov', 'A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.' );
--- tosca/creation-script-postgresql.sql
+++ tosca/creation-script-postgresql.sql
@@ -18,4 +18,7 @@ CREATE TABLE public.MESSAGES (
 GRANT ALL PRIVILEGES ON  ALL TABLES IN SCHEMA  public TO brooklyn;
 GRANT ALL PRIVILEGES ON  ALL SEQUENCES IN SCHEMA  public TO brooklyn;
 
-INSERT INTO MESSAGES(NAME, MESSAGE) values ( 'Isaac Asimov', 'I hope you took your vitamins' );
+INSERT INTO MESSAGES(NAME, MESSAGE) values ( 'Isaac Asimov', 'I was born in Brooklyn' );
+INSERT INTO MESSAGES(NAME, MESSAGE) values ( 'Isaac Asimov', 'A robot may not injure a human being or, through inaction, allow a human being to come to harm.' );
+INSERT INTO MESSAGES(NAME, MESSAGE) values ( 'Isaac Asimov', 'A robot must obey the orders given it by human beings except where such orders would conflict with the First Law.' );
+INSERT INTO MESSAGES(NAME, MESSAGE) values ( 'Isaac Asimov', 'A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.' );
